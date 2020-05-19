8ad5ce90164059638efb883763d86c0fbed7005b
鲁涛 <merrycodes@163.com>
2020-05-19 16:18:36 +0800

    :tada:Start writing exercise modules

---
+++ app/src/main/java/com/merrycodes/activity/ExerciseDetailActivity.java
@@ -0,0 +1,16 @@
+package com.merrycodes.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.merrycodes.R;
+
+public class ExerciseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_detail);
+    }
+}
